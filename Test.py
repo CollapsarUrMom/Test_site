8917fd5574d2c9a91ad4d2e8b71f21c227dfd32b
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2023-12-19 12:55:32 +0300

    Доделать!

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-print('123')
+dic = {}
+
+for _ in range(int(input())):
+    friend, data, month = input().split()
+    celendar = dic.setdefault(friend, [month, data])
+    print(celendar)
+    
+print(dic)
+
+
+for _ in range(int(input())):
+    input()
