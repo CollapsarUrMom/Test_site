8446aa214314418efcf0a6195ffd138a45e18f18
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2023-12-19 20:18:32 +0300

    Обед!

--- Test.py
+++ Test.py
@@ -1,1 +1,25 @@
-print('sdfsdf')
+dic = {}
+result = []
+
+for _ in range(int(input())):
+    friend, data, month = input().split()
+    celendar = dic.setdefault(friend, month)
+    #print(celendar)
+    
+print(dic)
+print(dic.values())
+
+
+for _ in range(int(input())):
+    a = input()
+    for key in dic:
+        if dic[key] == a:
+            result.append(key)
+
+print(result)
+
+#keys = [key for key in students if students[key] == target_grade]
+
+
+
+
