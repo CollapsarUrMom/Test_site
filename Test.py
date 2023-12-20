c3d9dde9f7bc164d3e9c66b7d53261b0a728c8ab
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2023-12-20 18:15:33 +0300

    1

--- Test.py
+++ Test.py
@@ -1,25 +1,9 @@
 dic = {}
-result = []
 
-for _ in range(int(input())):
-    friend, data, month = input().split()
-    celendar = dic.setdefault(friend, month)
-    #print(celendar)
+while input() != 'конец':
+    a = input()
+    taxi_driver, rating = a.split()
+    dic[taxi_driver] = rating
     
 print(dic)
-print(dic.values())
-
-
-for _ in range(int(input())):
-    a = input()
-    for key in dic:
-        if dic[key] == a:
-            result.append(key)
-
-print(result)
-
-#keys = [key for key in students if students[key] == target_grade]
-
-
-
 
