898f0de2263696ebc48bd6fd24c5f873f6862e37
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2023-12-21 21:43:39 +0300

    1

--- Test.py
+++ Test.py
@@ -5,14 +5,11 @@ lst = [i.split(', ') for i in iter(input, 'конец')]
 for i in lst:
     dic.setdefault(i[0], []).append(int(i[1]))
 
+print(dic)
+
 for i in sorted(dic.keys()):
     res = sum(dic[i])/len(dic[i])
     dic[i] = res
 
-    
-my_lst = sorted(subject_marks, key = lambda valuation: valuation[0][0])
-
-[print(*i) for i in sorted(my_lst, key = lambda valuation: valuation[1], reverse = True)]
-
-
-[print(i[0], i[1]) for i in sorted(dic.items(), key= lambda x: (x[1], x[0]))]
+for i in sorted(dic.items(), key= lambda x: (-x[1], x[0])):
+    print(i[0], i[1])
