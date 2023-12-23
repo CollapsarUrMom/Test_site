7a07efc3882962e1b95f9444a9a22486adb7664d
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2023-12-23 18:28:25 +0300

    Вроде готово2

--- Job.py
+++ Job.py
@@ -9,9 +9,10 @@ diametr = 290 # Диаметр
 for i in range(start, end, step):
     x = (diametr / 2) * math.cos(rad * i)
     y = (diametr / 2) * math.sin(rad * i)
+    print(f'x {x} y {y} при {i} градусах')
+    
     x_next = (diametr / 2) * math.cos(rad * (i + step))
     y_next = (diametr / 2) * math.sin(rad * (i + step))
     processing_width = math.sqrt(((x - x_next) ** 2) + ((y - y_next) ** 2))
-    print(f'x {x} y {y} при {i} градусах')
     print(f'Максимальная ширина съёма {processing_width}')
     print()
