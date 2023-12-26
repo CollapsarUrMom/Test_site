f2502afa7449ba6a5d9e4e324b31e59fc9297b86
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2023-12-26 13:54:06 +0300

    1

--- Test.py
+++ Test.py
@@ -1,16 +1,32 @@
-#def add(a, b):
-    #return a + b
-
-def counter(func):
+def create_counter():
     count = 0
-    def inner(*args, **kwargs):
+
+    def increment(value: int = 1):
         nonlocal count
-        count += 1
-        print(f'функция {func.__name__} вызывалась {count} раз')
-        return func(*args,**kwargs)
+        count += value
+        return count
 
-    return inner
-q = counter(lambda a, b: a + b)
+    def decrement(value: int = 1):
+        nonlocal count
+        count -= value
+        return count
 
-q(10, 20)
-q(2,5)
+    return increment, decrement
+
+
+inc_1, dec_1 = create_counter()
+print(inc_1())  # увеличиваем на 1
+print(inc_1(2))  # увеличиваем на 2
+print(inc_1(3))  # увеличиваем на 3
+print(dec_1())  # уменьшаем на 1
+print(dec_1())  # уменьшаем на 1
+
+print('-' * 15)
+print('Создаем новый объект замыкания с другим счетчиком')
+
+inc_2, dec_2 = create_counter()
+print(inc_2(10))  # увеличиваем на 10
+print(dec_2(5))  # уменьшаем на 5
+print(inc_2(100))  # увеличиваем на 100
+print(inc_2(50))  # увеличиваем на 50
+print(dec_2())  # уменьшаем на 1
