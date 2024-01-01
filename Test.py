eacb48526291433f2b5ee94154ec8725d609661a
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-01-01 23:10:03 +0300

    1

--- Test.py
+++ Test.py
@@ -1,14 +1,9 @@
-def find_lines_len_more_6(file_name: str) -> int:
+def search_for_unique_words(file_name: str) -> int:
     with open(file_name, mode= 'r', encoding= 'UTF-8') as file:
-        count = 0
-        lines = [line.strip() for line in file]
-        for i in lines:
-            if len(i) > 6:
-                count += 1
-        return count
+        res = [i for i in file.read().lower().split()]
+        print(len(set(res)))
 
 
 
 
-
-print(find_lines_len_more_6('hello.txt'))
+search_for_unique_words('D:\\Alex\\Сегодня\\lorem.txt')
