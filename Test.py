7127f8b7e3079421e3d2109c7ebb0531bae0029e
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-01-04 13:12:37 +0300

    1

--- Test.py
+++ Test.py
@@ -1,8 +1,9 @@
 import json
 
-with open('D:\\Alex\\Сегодня\\manager_sales.json', 'r') as file:
-    print(type(file))
-    string = json.load(file)
-    print(type(string))
-    my_file = json.dumps(string, indent= 4)
-    
+with open('D:\\Alex\\Сегодня\\manager_sales.json', 'r') as file_json:
+    data = json.load(file_json)
+a = []
+for i in data:
+    a.append((i['manager']['first_name'], i['manager']['last_name'], sum(s['price'] for s in i['cars'])))
+
+print(*sorted(a, key=lambda x: x[2], reverse=True)[0])
