8dc9b7bf026bdd7f8c176c8bdb13ef929d5cc2ce
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-01-11 21:08:29 +0300

    1

--- Decoder.py
+++ Decoder.py
@@ -1,14 +1,25 @@
 import json
 count = 10
-with open('C:\\Users\\Alex_job\\Documents\\Program_job\\Copy.txt', mode= 'r', encoding= 'UTF-8') as program, open('C:\\Users\\Alex_job\\Documents\\Program_job\\Paste.txt', mode= 'w', encoding= 'UTF-8') as res:
+last_line = ''
+
+with open('C:\\Users\\Alex\\Downloads\\Copy.txt', mode= 'r', encoding= 'UTF-8') as program, open('C:\\Users\\Alex\\Downloads\\Paste.txt', mode= 'w', encoding= 'UTF-8') as res:
     file = program.readlines()
 
     for line in file:
-        if line[0] == 'N':
-            for letter in line:
-                if letter == 'N' or letter != '(':
-                    del[letter]
-            res.write('N' + str(count) + line + '\n')
-            count += 10
+        if last_line == '\n' or 'M1' in last_line or 'if' in last_line:
+            num = 1
+        else:
+            num = 10
+        if 'N' in line and '(' in line:
+            line = line.replace(line[line.index('N'):line.index('(')], 'N' + str(count))
+            res.write(line)
+            count += num
+        elif 'N['in line:
+            res.write(line)
+        elif 'N' in line:
+            line = line.replace(line, 'N' + str(count))
+            res.write(line)
+            count += num
         else:
             res.write(line)
+        last_line = line
