456ff1740e172d26161458f970cfcea556b1de54
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-01-12 02:52:29 +0300

    1

--- Decoder.py
+++ Decoder.py
@@ -1,5 +1,6 @@
 import json
-count = 10
+count_10 = (num for num in range(10, 1010, 10))
+count_1 = (num for num in range(1, 1001, 1))
 last_line = ''
 
 with open('C:\\Users\\Alex_job\\Documents\\Program_job\\Copy.txt', mode= 'r', encoding= 'UTF-8') as program, open('C:\\Users\\Alex_job\\Documents\\Program_job\\Paste.txt', mode= 'w', encoding= 'UTF-8') as res:
@@ -7,15 +8,12 @@ with open('C:\\Users\\Alex_job\\Documents\\Program_job\\Copy.txt', mode= 'r', en
 
     for line in file:
         if last_line == '\n' or 'M1' in last_line or 'if' in last_line or '(B270)' in last_line:
-            num = 1
-        elif 'GOTO' in last_line:
-            count = count - (count // 10)
+            count = next (count_10)
         else:
-            num = 10
+            count = next(count_1)
         if 'N' in line and '(' in line:
             line = line.replace(line[line.index('N'):line.index('(')], 'N' + str(count))
             res.write(line)
-            count += num
         elif 'N['in line:
             res.write(line)
         elif 'END' in line:
@@ -23,7 +21,6 @@ with open('C:\\Users\\Alex_job\\Documents\\Program_job\\Copy.txt', mode= 'r', en
         elif 'N' in line:
             line = line.replace(line, 'N' + str(count))
             res.write(line)
-            count += num
         else:
             res.write(line)
         last_line = line
