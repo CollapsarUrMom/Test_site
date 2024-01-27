1e54e25caa10be7303066181bbc74d8b47b55e35
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-01-27 10:01:02 +0300

    1

--- Test.py
+++ Test.py
@@ -1,24 +1,9 @@
-# Напишите определение функции zip_with_function
-def zip_with_function(lst, f) -> list[str, int]:
-    return list(map(lambda args: f(*args), zip(*lst)))
+def find_keys(*args, **kwargs):
+    for i in kwargs.items():
+        print(type(i[1]))
+        if isinstance(i[1], list):
+            print(i[0])
 
-
-
-
-def combine_strings(a: str, b: str) -> str:
-    return a + b
-
-
-def get_sum_two_numbers(a: int, b: int) -> int:
-    return a + b
-
-
-def get_sum_three_numbers(a: int, b: int, c: int) -> int:
-    return a + b + c
-
-
-assert zip_with_function([[1, 2, 4], [3, 5, 8]], get_sum_two_numbers) == [4, 7, 12]
-assert zip_with_function([[10, 20], [30, 0]], get_sum_two_numbers) == [40, 20]
-assert zip_with_function([[2, 5, 8], [3, 4, 7], [5, 6, 5]], get_sum_three_numbers) == [10, 15, 20]
-assert zip_with_function([[1, 2, 3], [4, 5, 6], [7, 8, 9]], get_sum_three_numbers) == [12, 15, 18]
-assert zip_with_function([["a", "b"], ["1", "2"]], combine_strings) == ['a1', 'b2']
+assert find_keys(t=[4, 5], W=[5, 3], A=(3, 2), a={2, 3}, b=[4]) == ['A', 'b', 't', 'W']
+assert find_keys(name='Bruce', surname='Wayne') == []
+assert find_keys(marks=[4, 5], name='ashle', surname='Brown', age=20, Also=(1, 2)) == ['Also', 'marks']
