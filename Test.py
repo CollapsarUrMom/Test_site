08272a36619a5152b10d7d25ace2bf4d14793c78
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-01-27 18:09:52 +0300

    1

--- Test.py
+++ Test.py
@@ -1,9 +1,6 @@
-def find_keys(*args, **kwargs):
-    for i in kwargs.items():
-        print(type(i[1]))
-        if isinstance(i[1], list):
-            print(i[0])
+a = list(map(int, input().split()))
 
-assert find_keys(t=[4, 5], W=[5, 3], A=(3, 2), a={2, 3}, b=[4]) == ['A', 'b', 't', 'W']
-assert find_keys(name='Bruce', surname='Wayne') == []
-assert find_keys(marks=[4, 5], name='ashle', surname='Brown', age=20, Also=(1, 2)) == ['Also', 'marks']
+if a == sorted(a, reverse= True) and a[:1] != a[-1:]:
+    print(True)
+else:
+    print(False)
