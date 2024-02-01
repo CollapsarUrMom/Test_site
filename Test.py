db9d6436b6478774a2f4a343e0cecd6a2bc612f0
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-02-01 06:10:49 +0300

    1

--- Test.py
+++ Test.py
@@ -1,14 +1,34 @@
+# Напишите определение класса Robot       
 class Robot:
+
     def say_hello(self):
-        print('Hello, human! My name is C-3PO')
+        if hasattr(self, 'name'):
+            print(f'Hello, human! My name is', getattr(self, 'name'))
+        else:
+            print('У робота нет имени')
+
         
     def say_bye(self):
-        print('See you later alligator')
+        print('See u later alligator')
+
+    def set_name(my_robot, name):
+        my_robot.name = name
+        return my_robot.name
+
+# Ниже код для проверки класса Robot 
 
 c3po = Robot()
-r2d2 = Robot()
-
+c3po.say_hello()
+c3po.set_name('R2D2')
 c3po.say_hello()
 c3po.say_bye()
-r2d2.say_hello()
-r2d2.say_bye()
+
+r = Robot()
+r.set_name('Chappy')
+r.say_hello()
+
+d = Robot()
+d.say_hello()
+d.set_name('Wally')
+d.say_hello()
+print('========================================')
