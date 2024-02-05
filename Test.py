a6d434b3371a245f4500803464947a6535775004
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-02-05 21:57:30 +0300

    1

--- Test.py
+++ Test.py
@@ -1,34 +1,33 @@
-# Напишите определение класса Constructor       
-class Constructor():
+class Point:
+    
+    def set_coordinates(point_1, x, y):
+        point_1.x = x
+        point_1.y = y
 
-    def add_atribute(self, key, value):
-        setattr(self, key, value)
+    def get_distance(point_1, point_2):
+        if isinstance(point_2, Point):
+            return ((point_1.x - point_2.x) ** 2 + (point_1.y - point_2.y) ** 2) ** 0.5
+        else:
+            print('Передана не точка')
 
-    def display(self):
-        print(self.__dict__)
+# Код ниже не удаляйте, он нужен для проверок
+p1 = Point()
+p2 = Point()
+assert isinstance(p1, Point)
+assert isinstance(p2, Point)
 
-# Ниже код для проверки класса Constructor 
+p1.set_coordinates(1, 2)
+assert p1.x == 1
+assert p1.y == 2
+p2.set_coordinates(4, 6)
+assert p2.x == 4
+assert p2.y == 6
+assert p1.get_distance(p2) == 5.0
+p3 = Point()
+p3.set_coordinates(10, 10)
+p1.set_coordinates(4, 2)
+assert p1.get_distance(p3) == 10.0
+res = p1.get_distance(10)  # Распечатает "Передана не точка", вернет None
+assert res is None, 'Метод get_distance должен возвращать None, если в него была передана не точка'
 
-obj1 = Constructor()
-assert obj1.__dict__ == {}
-obj1.display()
-obj1.add_atribute('color', 'red')
-assert obj1.color == 'red'
-obj1.add_atribute('width', 20)
-assert obj1.width == 20
-obj1.display()
-
-obj2 = Constructor()
-obj2.display()
-obj2.add_atribute('height', 100)
-assert obj2.height == 100
-obj2.display()
-
-obj3 = Constructor()
-obj3.display()
-obj3.add_atribute('a', 100)
-obj3.add_atribute('b', 300)
-obj3.add_atribute('c', 200)
-obj3.add_atribute('b', 1)
-assert obj3.__dict__ == {'a': 100, 'b': 1, 'c': 200}
-obj3.display()
+assert p2.get_distance([1, 2, 3]) is None  # Распечатает "Передана не точка", вернет None
