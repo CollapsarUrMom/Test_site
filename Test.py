eec84c60fcd0583e2496aeb39ac6b4f234e586bb
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-02-26 21:27:07 +0300

    1

--- Test.py
+++ Test.py
@@ -1,48 +1,81 @@
-class Numbers:
+# Напишите определение класса Stack
+class Stack:
 
-    def __init__(self, *args):
-        self.lst = list(args)
+    def __init__(self, values= []):
+        self.values = list(values)
 
-    def get_positive(self):
-        return [i for i in self.lst if i > 0]
+    def push(self, item):
+        self.values.append(item)
+
+    def pop(self):
+        if self.values == []:
+            print('Empty Stack')
+        self.values.[-1]
+
+    def peek(self):
+        if self.values == []:
+            print('Empty Stack')
+            return None
+        else:
+            return self.values[-1]
+        
+    def is_empty(self):
+        if self.values == []:
+            return True
+        return False
     
-    def get_negative(self):
-        return [i for i in self.lst if i < 0]
-    
-    def get_zeroes(self):
-        return [i for i in self.lst if i == 0]
-    
-    def add_number(self, num):
-        self.lst.append(num)
-    
+    def size(self):
+        return len(self.values)
 
-nums = Numbers(1, 2, -4, -5, 3)
-assert nums.lst == [1, 2, -4, -5, 3]
-assert nums.get_positive() == [1, 2, 3]
-assert nums.get_negative() == [-4, -5]
+# Ниже код для проверки класса Stack
 
-nums2 = Numbers(10, 20, 30, 0, 0, 5)
+s = Stack()
+assert s.values == []
+assert isinstance(s, Stack)
 
-assert nums2.get_positive() == [10, 20, 30, 5]
-assert nums2.get_zeroes() == [0, 0]
-nums2.add_number(100)
-nums2.add_number(0)
-nums2.add_number(7)
-assert nums2.get_positive() == [10, 20, 30, 5, 100, 7]
-assert nums2.get_zeroes() == [0, 0, 0]
+s.peek()  # распечатает 'Empty Stack'
+assert s.is_empty() is True
+s.push('cat')
+assert s.size() == 1
+assert s.peek() == 'cat'
 
-nums = Numbers(7, 8, 9)
-nums_2 = Numbers(7, 8, 9)
+s.push('dog')
+assert s.size() == 2
+assert s.peek() == 'dog'
 
-nums.add_number(10)
-nums_2.add_number(11)
-nums_2.add_number(12)
-assert nums.get_positive() == [7, 8, 9, 10]
-assert nums_2.get_positive() == [7, 8, 9, 11, 12]
+s.push(True)
+assert s.size() == 3
+assert s.is_empty() is False
 
-nums_3 = Numbers(-1, -2, -3, 0, -6, -4, 0, 0)
+s.push(777)
+assert s.size() == 4
 
-assert nums_3.get_positive() == []
-assert nums_3.get_negative() == [-1, -2, -3, -6, -4]
-assert nums_3.get_zeroes() == [0, 0, 0]
-print('Good')
+assert s.pop() == 777
+assert s.size() == 3
+
+assert s.pop() is True
+assert s.size() == 2
+
+s.push(123)
+s.push(123456)
+assert s.peek() == 123456
+assert s.size() == 4
+
+assert s.pop() == 123456
+assert s.pop() == 123
+assert s.pop() == 'dog'
+assert s.is_empty() is False
+assert s.pop() == 'cat'
+assert s.is_empty() is True
+
+
+d = Stack()
+assert d.peek() is None  # Печатает "Empty Stack"
+assert d.pop() is None  # Печатает "Empty Stack"
+d.push('hello')
+assert d.size() == 1
+d.push('world')
+assert d.size() == 2
+assert d.peek() == 'world'
+assert d.pop() == 'world'
+assert d.peek() == 'hello'
