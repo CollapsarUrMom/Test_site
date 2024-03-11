28f75de734e81607089bc7f4d30180209418d0b4
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-03-11 05:56:57 +0300

    1

--- programma_dly_obrabotki_chekov/example.py
+++ programma_dly_obrabotki_chekov/example.py
@@ -1,10 +1,17 @@
 import openpyxl
 import json
 
+#===============================================
+#  Json файла с чеками
+
 with open('C:\\Users\\Alex_job\\Documents\\Git\\Test_site\\programma_dly_obrabotki_chekov\\extract.json', mode= 'r', encoding= 'UTF-8') as file:
     my_file = json.load(file)
     new_json = json.dumps(my_file, ensure_ascii=False, indent= 4)
     my_json = json.loads(new_json)
+
+
+#===============================================
+#  Сделал для теста    
 
 cheque = my_json[0]
 
@@ -15,7 +22,13 @@ quantity = cheque["ticket"]["document"]["receipt"]["items"][0]["quantity"]
 
 count_items = len(cheque["ticket"]["document"]["receipt"]["items"])
 
+
+#===============================================
+#  Цикл создания словаря из нужных значений
+
 dict_product = {}
+list_product = []
+
 
 for i in range(count_items):
     #dict_product = {cheque["ticket"]["document"]["receipt"]["items"][i]["name"] : [cheque["ticket"]["document"]["receipt"]["items"][i]["price"], cheque["ticket"]["document"]["receipt"]["items"][i]["quantity"]]}
@@ -23,7 +36,9 @@ for i in range(count_items):
                                                                                   cheque["ticket"]["document"]["receipt"]["items"][i]["quantity"], 
                                                                                   cheque['createdAt'][0:-15]]
 
-print(dict_product)
+
+#===============================================
+#  Кадры для созданрия файла exel и его заполнения (шаблон)
 
 #wb = Workbook()
 #income = wb.active
@@ -39,23 +54,59 @@ celendar = {1:'c', 2:'d', 3:'e', 4:'f', 5:'g', 6:'h', 7:'i', 8:'j', 9:'k', 10:'l
              24:'z', 25:'aa', 26:'ab', 27:'ac', 28:'ad', 29:'ae', 30:'af', 31:'ag'}
 #print(wb.sheetnames)
 
+#===============================================
+#  Открытие сущетвующего файла exel и его наполнение чеками
 
 wb = openpyxl.load_workbook(filename= 'test.xlsx')
 
 expenses = wb['Расходы']
 income = wb['Доходы']
 
-i = 3
+count = 2
 
 for product in dict_product.keys():
     month = int(dict_product[product][2][5:7])
     day = int(dict_product[product][2][8:])
-    expenses[f'B{i}'] = product
-    expenses[f'{celendar[day]}{i}'] = dict_product[product][0] #price
+    if expenses[f'B{count}'] != None:
+        expenses[f'B{count}'] = product
+        expenses[f'{celendar[day]}{count}'] = dict_product[product][0] #price
     i += 1
 
-#expenses.move_range("A4:C6", rows= -1, cols= 1)
 
+class Check:
+
+    count = 2
+    
+    def __init__(self, check):
+        self.dict_product = check
+        self.scaning_products(self)
+
+    def scaning_products(self, element):
+        for key in self.dict_product:
+            if key in list_product:
+                self.add_price(self, count)
+            else:
+                list_product.append(key)
+                self.cell_search()
+                
+
+    def add_price(self, count):
+        pass#expenses[f'{celendar[day]}{count}'] = dict_product[product][0]
+
+    def cell_search(self):
+        count = 2
+        while expenses[f'B{count}'].value is not None:
+            count += 1
+        self.add_price(self, count)
+
+
+ch = Check(dict_product)
+#ch.adding_elements(10)
+print(ch)
+
+
+
+        
 
 wb.save('test.xlsx')
 wb.close()
