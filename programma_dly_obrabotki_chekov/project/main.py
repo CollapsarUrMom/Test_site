75e11e6baa76f3244f8fec84b9e9f689f275d319
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-04-03 06:16:20 +0300

    v2.0 Пробую использовать экземпляры класса для юольшей гибкости программы.

--- programma_dly_obrabotki_chekov/project/main.py
+++ programma_dly_obrabotki_chekov/project/main.py
@@ -29,44 +29,20 @@ income = wb['Доходы']
 
 
 class Check:
-    
-    def __init__(self, check):
-        self.dict_product = check
-        self.scaning_products(self)
 
-    def scaning_products(self, element):
-        month_number = int(dict_product[list(dict_product.keys())[0]][2][5:7])
-        day_number = int(dict_product[list(dict_product.keys())[0]][2][8:])
-        month_line = self.search_month_number(celendar_month[month_number])
-        for key in self.dict_product:
-            line = self.product_search(key, month_line)
-            if expenses[f'B{line}'].value == key:
-                expenses[f'{celendar_day[day_number]}{line}'] = dict_product[key][0] * dict_product[key][1]
-            else:
-                expenses[f'B{line}'] = key
-                expenses[f'{celendar_day[day_number]}{line}'] = dict_product[key][0] * dict_product[key][1]
-                expenses.insert_rows(line + 1, 1)
+    def __init__(self, name, data, price, quantity) -> None:
+        self.name = name
+        self.data = data
+        self.price = price
+        self.quantity = quantity
 
-    def search_month_number(self, month):
-        count = 1
-        while expenses[f'A{count}'].value != month:
-            count += 1
-        return count + 1
-    
-    def product_search(self, key, month):
-        count = month
-        while expenses[f'B{count}'].value != key and expenses[f'B{count}'].value is not None:
-            count += 1
-        return count
 
-for i in range(len(my_json)):
-    cheque = my_json[i]
-    dict_product = {}
-    for i in range(len(cheque["ticket"]["document"]["receipt"]["items"])):
-        dict_product[cheque["ticket"]["document"]["receipt"]["items"][i]["name"]] = [cheque["ticket"]["document"]["receipt"]["items"][i]["price"] / 100,
-                                                                                      cheque["ticket"]["document"]["receipt"]["items"][i]["quantity"],
-                                                                                        cheque["ticket"]["document"]["receipt"]['dateTime'][0 : 10]]
-    ch = Check(dict_product)
+class CheckList:
+
+    def __init__(self) -> None:
+        pass
+print(my_json[0])
+purchases = Check()
   
 
 wb.save('C:\\Users\\Alex_job\\Documents\\Git\\Test_site\\programma_dly_obrabotki_chekov\\my_finances.xlsx')
