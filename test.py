b3e8a76a1d036d261c1cccb7aaee1f6ae0fe6d9e
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-04-12 22:13:52 +0300

    1

--- test.py
+++ test.py
@@ -1,27 +1,12 @@
-def handle_case_1():
-    print("Создание базы дааных")
+class PizzaMaker:
+    def __make_pepperoni(self):
+        pass
 
-def handle_case_2():
-    print("Создание объекта")
+    def _make_barbecue(self):
+        pass
 
-def handle_case_3():
-    print("Добавление данных в базу данных")
 
-def handle_case_4():
-    print("Конец")
-
-def handle_default():
-    print("Нет такой команды")
-
-switch_case = {
-    1: handle_case_1, # Создание базы данных
-    2: handle_case_2, # Добавь данные в базу данных
-    3: handle_case_3, # Добавление данных в базу данных
-    4: handle_case_4, # Конец
-}
-condition = '1'
-
-while condition != 4:
-    condition = int(input("Введите команду: "))
-    action = switch_case.get(condition, handle_default)
-    action()
+maker = PizzaMaker()
+maker._make_barbecue()
+print(PizzaMaker.__dict__.keys())
+maker._PizzaMaker__make_pepperoni()
