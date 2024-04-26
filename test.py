b5823efa18fb09301a02669e6d0df3244263aa33
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-04-26 06:02:31 +0300

    1

--- test.py
+++ test.py
@@ -1,43 +1,26 @@
-# Напишите определение класса Employee       
-class Employee:
+# Напишите определение класса BankAccount       
+class BankAccount:
 
-    def __init__(self, name, position, hours_worked,  hourly_rate) -> None:
-        self.name = name
-        self.__position = position
-        self.__hours_worked = hours_worked
-        self.__hourly_rate = hourly_rate
+    def __init__(self, _account_number, _balance) -> None:
+        self._account_number = _account_number
+        self._balance = _balance
 
-    def __calculate_salary(self):
-        return self.__hours_worked * self.__hourly_rate
-    
-    def _set_position(self, position):
-        self.__position = position
+    def get_account_number(self):
+        return self._account_number
 
-    def get_position(self):
-        return self.__position
-    
-    def get_salary(self):
-        return self.__calculate_salary()
-    
-    def get_employee_details(self):
-        return f'Name: {self.name}, Position: {self.__position}, Salary: {self.get_salary()}'
-    
+    def get_balance(self):
+        return self._balance
 
-# Ниже код для проверки методов класса Employee
-employee = Employee("Джеки Чан", 'manager', 20, 40)
-assert employee.name == 'Джеки Чан'
-assert employee._Employee__hours_worked == 20
-assert employee._Employee__hourly_rate == 40
-assert employee._Employee__position == 'manager'
-assert employee.get_position() == 'manager'
-assert employee.get_salary() == 800
-assert employee._Employee__calculate_salary() == 800
-assert employee.get_employee_details() == 'Name: Джеки Чан, Position: manager, Salary: 800'
-employee._set_position('Director')
-assert employee.get_employee_details() == 'Name: Джеки Чан, Position: Director, Salary: 800'
+    def set_balance(self, value):
+        self._balance = value
 
-employee_2 = Employee("Пирс Броснан", 'actor', 35, 30)
-assert employee_2._Employee__calculate_salary() == 1050
-assert employee_2.get_employee_details() == 'Name: Пирс Броснан, Position: actor, Salary: 1050'
+# Ниже код для проверки методов класса BankAccount
+account = BankAccount("1234567890", 1000)
+assert account._balance == 1000
+assert account._account_number == "1234567890"
+assert account.get_account_number() == "1234567890"
+assert account.get_balance() == 1000
+account.set_balance(1500)
+assert account.get_balance() == 1500
 
 print('Good')
