e441c38d598ac5391d0ed99872700e15a845eb18
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-05-17 05:38:56 +0300

    1

--- test.py
+++ test.py
@@ -1,36 +1,33 @@
-# Напишите определение класса Date       
-class Date:
+# Напишите определение класса TemperatureConverter       
+class TemperatureConverter:
 
-    def __init__(self, d, m, y) -> None:
-        self.day = d
-        self.month = m
-        self.year = y
+    def __init__(self) -> None:
+        pass
 
-    @property
-    def date(self):
-        return f'{self.day:02}/{self.month:02}/{self.year:04}'
-    
-    @property
-    def usa_date(self):
-        return f'{self.month:02}-{self.day:02}-{self.year:04}'
+# Ниже код для проверки методов класса TemperatureConverter
+assert TemperatureConverter.celsius_to_fahrenheit(0) == 32.0
+assert TemperatureConverter.celsius_to_fahrenheit(10) == 50.0
+assert TemperatureConverter.celsius_to_fahrenheit(15) == 59.0
+assert TemperatureConverter.celsius_to_fahrenheit(20) == 68.0
+assert TemperatureConverter.celsius_to_fahrenheit(25) == 77.0
+assert TemperatureConverter.celsius_to_fahrenheit(30) == 86.0
 
-# Ниже код для проверки методов класса Date
+assert TemperatureConverter.fahrenheit_to_celsius(86) == 30.0
+assert TemperatureConverter.fahrenheit_to_celsius(77) == 25.0
+assert TemperatureConverter.fahrenheit_to_celsius(68) == 20.0
+assert TemperatureConverter.fahrenheit_to_celsius(59) == 15.0
+assert TemperatureConverter.fahrenheit_to_celsius(50) == 10.0
+assert TemperatureConverter.fahrenheit_to_celsius(32) == 0
 
-d1 = Date(5, 10, 2001)
-assert isinstance(d1, Date)
-assert d1.date == '05/10/2001'
-assert d1.usa_date == '10-05-2001'
-assert isinstance(type(d1).date, property), 'Вы не создали property date'
-print(d1.date, d1.usa_date)
+converter = TemperatureConverter()
+assert converter.celsius_to_kelvin(100) == 373.15
+assert converter.kelvin_to_celsius(273.15) == 0
+assert converter.fahrenheit_to_kelvin(50) == 283.15
+assert converter.fahrenheit_to_kelvin(134.33) == 330.0
+assert converter.kelvin_to_fahrenheit(54.0) == -362.47
+assert converter.kelvin_to_fahrenheit(1653.0) == 2515.73
+assert converter.format(1653.0, 'K') == '1653.0°K'
+assert converter.format(45, 'F') == '45°F'
+assert converter.format(36.6, 'C') == '36.6°C'
 
-d2 = Date(15, 3, 999)
-assert isinstance(d2, Date)
-assert d2.date == '15/03/0999'
-assert d2.usa_date == '03-15-0999'
-assert isinstance(type(d2).date, property), 'Вы не создали property date'
-print(d2.date, d2.usa_date)
-
-d3 = Date(3, 5, 3)
-assert d3.date == '03/05/0003'
-assert d3.usa_date == '05-03-0003'
-print(d3.date, d3.usa_date)
+print('Good')
