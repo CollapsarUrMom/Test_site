653770c70a47300278b36a527a0c151815e6e3b8
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-05-21 22:29:23 +0300

    1

--- test.py
+++ test.py
@@ -1,33 +1,52 @@
-# Напишите определение класса TemperatureConverter       
-class TemperatureConverter:
+import json
 
-    def __init__(self) -> None:
-        pass
 
-# Ниже код для проверки методов класса TemperatureConverter
-assert TemperatureConverter.celsius_to_fahrenheit(0) == 32.0
-assert TemperatureConverter.celsius_to_fahrenheit(10) == 50.0
-assert TemperatureConverter.celsius_to_fahrenheit(15) == 59.0
-assert TemperatureConverter.celsius_to_fahrenheit(20) == 68.0
-assert TemperatureConverter.celsius_to_fahrenheit(25) == 77.0
-assert TemperatureConverter.celsius_to_fahrenheit(30) == 86.0
+# Напишите определение класса AppConfig
+class AppConfig:
 
-assert TemperatureConverter.fahrenheit_to_celsius(86) == 30.0
-assert TemperatureConverter.fahrenheit_to_celsius(77) == 25.0
-assert TemperatureConverter.fahrenheit_to_celsius(68) == 20.0
-assert TemperatureConverter.fahrenheit_to_celsius(59) == 15.0
-assert TemperatureConverter.fahrenheit_to_celsius(50) == 10.0
-assert TemperatureConverter.fahrenheit_to_celsius(32) == 0
+    @classmethod
+    def load_config(cls, database):
+        with open('C:\\Users\\Alex_job\\Documents\\Git\\Test_site\\app_config.json') as database:
+            cls.data = json.load(database)
+            return cls.data
+        
+    @classmethod
+    def get_config(cls, k : int = None):
+        key = k.split('.')
+        print(key)
+        if len(key) == 1:
+            return cls.data[key[0]]
+        elif len(key) == 2:
+            print(key)
+            print(key[0])
+            print(type(key))
+            print(type(key[0]))
+            return cls.data.get(key[0]key[1], None)
 
-converter = TemperatureConverter()
-assert converter.celsius_to_kelvin(100) == 373.15
-assert converter.kelvin_to_celsius(273.15) == 0
-assert converter.fahrenheit_to_kelvin(50) == 283.15
-assert converter.fahrenheit_to_kelvin(134.33) == 330.0
-assert converter.kelvin_to_fahrenheit(54.0) == -362.47
-assert converter.kelvin_to_fahrenheit(1653.0) == 2515.73
-assert converter.format(1653.0, 'K') == '1653.0°K'
-assert converter.format(45, 'F') == '45°F'
-assert converter.format(36.6, 'C') == '36.6°C'
+
+
+# Загрузка конфигурации при запуске приложения
+AppConfig.load_config('app_config.json')
+
+# Получение значения конфигурации
+assert AppConfig.get_config('database') == {
+    'host': '127.0.0.1', 'port': 5432,
+    'database_name': 'postgres_db',
+    'user': 'owner',
+    'password': 'ya_vorona_ya_vorona'}
+assert AppConfig.get_config('database.user') == 'owner'
+assert AppConfig.get_config('database.password') == 'ya_vorona_ya_vorona'
+assert AppConfig.get_config('database.pass') is None
+assert AppConfig.get_config('password.database') is None
+
+config = AppConfig()
+assert config.get_config('max_connections') == 10
+assert config.get_config('min_connections') is None
+
+conf = AppConfig()
+assert conf.get_config('max_connections') == 10
+assert conf.get_config('database.user') == 'owner'
+assert conf.get_config('database.host') == '127.0.0.1'
+assert conf.get_config('host') is None
 
 print('Good')
