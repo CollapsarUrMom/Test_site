841082e013686f1c37691163ce152470d3d73751
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-05-21 09:39:16 +0300

    1

--- test.py
+++ test.py
@@ -1,33 +1,32 @@
-# Напишите определение класса TemperatureConverter       
-class TemperatureConverter:
+class RobotVacuumCleaner:
+    name = 'Henry'
+    charge = 25
 
-    def __init__(self) -> None:
-        pass
+    @classmethod
+    def update_charge(cls, new_value):
+        cls.charge = new_value
 
-# Ниже код для проверки методов класса TemperatureConverter
-assert TemperatureConverter.celsius_to_fahrenheit(0) == 32.0
-assert TemperatureConverter.celsius_to_fahrenheit(10) == 50.0
-assert TemperatureConverter.celsius_to_fahrenheit(15) == 59.0
-assert TemperatureConverter.celsius_to_fahrenheit(20) == 68.0
-assert TemperatureConverter.celsius_to_fahrenheit(25) == 77.0
-assert TemperatureConverter.celsius_to_fahrenheit(30) == 86.0
+    def hello(name):
+        return f'Привет, {name}'
 
-assert TemperatureConverter.fahrenheit_to_celsius(86) == 30.0
-assert TemperatureConverter.fahrenheit_to_celsius(77) == 25.0
-assert TemperatureConverter.fahrenheit_to_celsius(68) == 20.0
-assert TemperatureConverter.fahrenheit_to_celsius(59) == 15.0
-assert TemperatureConverter.fahrenheit_to_celsius(50) == 10.0
-assert TemperatureConverter.fahrenheit_to_celsius(32) == 0
+    def data(self):
+        return {
+            'name': self.name,
+            'charge': self.charge
+        }
 
-converter = TemperatureConverter()
-assert converter.celsius_to_kelvin(100) == 373.15
-assert converter.kelvin_to_celsius(273.15) == 0
-assert converter.fahrenheit_to_kelvin(50) == 283.15
-assert converter.fahrenheit_to_kelvin(134.33) == 330.0
-assert converter.kelvin_to_fahrenheit(54.0) == -362.47
-assert converter.kelvin_to_fahrenheit(1653.0) == 2515.73
-assert converter.format(1653.0, 'K') == '1653.0°K'
-assert converter.format(45, 'F') == '45°F'
-assert converter.format(36.6, 'C') == '36.6°C'
+    def make_clean(self):
+        if self.charge < 30:
+            return 'Кожаный, заряди меня! Я слаб'
+        return 'Я вычищу твою берлогу!!!'
 
-print('Good')
+# код ниже не нужно удалять, в нем находятся проверки
+print(RobotVacuumCleaner.hello('Господин'))
+RobotVacuumCleaner.update_charge(50)
+
+robot = RobotVacuumCleaner()
+print(robot.make_clean())
+print(robot.data)
+
+RobotVacuumCleaner.update_charge(False)
+print(robot.make_clean())
