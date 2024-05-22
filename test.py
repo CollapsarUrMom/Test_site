fab10bf3a9f1742b92b8932fa52ce28aee9be59a
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-05-22 12:18:35 +0300

    1

--- test.py
+++ test.py
@@ -5,23 +5,23 @@ import json
 class AppConfig:
 
     @classmethod
-    def load_config(cls, database):
-        with open('C:\\Users\\Alex_job\\Documents\\Git\\Test_site\\app_config.json') as database:
+    def load_config(cls, database) -> dict:
+        with open('app_config.json') as database:
             cls.data = json.load(database)
             return cls.data
-        
+
+
     @classmethod
-    def get_config(cls, k : int = None):
-        key = k.split('.')
-        print(key)
-        if len(key) == 1:
-            return cls.data[key[0]]
-        elif len(key) == 2:
-            print(key)
-            print(key[0])
-            print(type(key))
-            print(type(key[0]))
-            return cls.data.get(key[0]key[1], None)
+    def get_config(cls, key : str = None):
+        keys = key.split('.')
+        if len(keys) == 1:
+            return cls.data.get(keys[0], None)
+        elif len(keys) == 2:
+            key_1 = cls.data.get(keys[0], None)
+            if key_1 is not None:
+                return key_1.get(keys[1], None)
+            else:
+                return None
 
 
 
@@ -48,3 +48,5 @@ assert conf.get_config('max_connections') == 10
 assert conf.get_config('database.user') == 'owner'
 assert conf.get_config('database.host') == '127.0.0.1'
 assert conf.get_config('host') is None
+
+print('Good')
