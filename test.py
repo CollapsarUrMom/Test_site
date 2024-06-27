c7c27f0044b425d14d8d866abfba7d23288197b5
Alexandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-06-27 05:48:10 +0300

    1

--- test.py
+++ test.py
@@ -1,57 +1,63 @@
-class Vector:
+# Напишите определение класса Rectangle       
+from functools import total_ordering
 
-    def __init__(self, *num):
-        lst = []
-        [lst.append(i) for i in num if isinstance(i, int)]
-        self.values = sorted(lst)
+@total_ordering
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
-    def __str__(self):
-        if self.values:
-            my_lst = [str(i) for i in self.values]
-            return f'Вектор({", ".join(my_lst)})'
-        else:
-            return 'Пустой вектор'
+    @property
+    def area(self):
+        return self.width * self.height
+
+
+    def __eq__(self, value):
+        if isinstance(value, Rectangle):
+            return self.area == value.area
+        elif isinstance(value, (int, float)):
+            return self.area == value
         
 
-    def __add__(self, other_vector):
-        if isinstance(other_vector, int):
-            lst_int = [num + other_vector for num in self.values]
-            return Vector(*lst_int)
-        elif isinstance(other_vector, Vector):
-            if len(other_vector.values) == len(self.values):
-                lst_vec = [sum(i) for i in zip(self.values, other_vector.values)]
-                return Vector(*lst_vec)
-            else:
-                print('Сложение векторов разной длины недопустимо')
-        else:
-            print(f'Вектор нельзя сложить с {other_vector}')
+    def __lt__(self, value):
+        if isinstance(value, Rectangle):
+            return self.area < value.area
+        elif isinstance(value, (int, float)):
+            return self.area < value
 
 
-    def __mul__(self, other_vector):
-        if isinstance(other_vector, int):
-            lst_mul = [num * other_vector for num in self.values]
-            return Vector(*lst_mul)
-        elif isinstance(other_vector, Vector):
-            if len(other_vector.values) == len(self.values):
-                lst_vec = [i[0] * i[1] for i in zip(self.values, other_vector.values)]
-                return Vector(*lst_vec)
-            else:
-                print('Умножение векторов разной длины недопустимо')
-        else:
-            print(f'Вектор нельзя умножать с {other_vector}')
+# Ниже код для проверки методов класса Rectangle
 
+r1 = Rectangle(3, 4)
+assert r1.width == 3
+assert r1.height == 4
+assert r1.area == 12
+assert isinstance(type(r1).area, property), 'Вы не создали property area'
 
+assert r1 > 11
+assert not r1 > 12
+assert r1 >= 12
+assert r1 <= 12
+assert not r1 > 13
+assert not r1 == 13
+assert r1 != 13
+assert r1 == 12
 
-v1 = Vector(1, 2, 3)
-print(v1) # печатает "Вектор(1, 2, 3)"
+r2 = Rectangle(2, 6)
+assert r1 == r2
+assert not r1 != r2
+assert not r1 > r2
+assert not r1 < r2
+assert r1 >= r2
+assert r1 <= r2
 
-v2 = Vector(3,4,5)
-print(v2) # печатает "Вектор(3, 4, 5)"
-v3 = v1 + v2
-print(v3) # печатает "Вектор(4, 6, 8)"
-v4 = v3 + 5
-print(v4) # печатает "Вектор(9, 11, 13)"
-v5 = v1 * 2
-print(v5) # печатает "Вектор(2, 4, 6)"
-v5 + 'hi' # печатает "Вектор нельзя сложить с hi"
+r3 = Rectangle(5, 2)
+assert not r2 == r3
+assert r2 != r3
+assert r2 > r3
+assert not r2 < r3
+assert r2 >= r3
+assert not r2 <= r3
+print('Good')
