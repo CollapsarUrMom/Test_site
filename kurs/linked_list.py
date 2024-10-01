a5ee091cec6f5bf2efcd24b9b7e7bcab1e9bfac2
Alexsandr Matveevskiy <Matveevskiy4721@gmail.com>
2024-10-01 20:18:46 +0300

    1

---
+++ kurs/linked_list.py
@@ -0,0 +1,177 @@
+# Создайте класс Node для создания узла
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Создайте класс LinkedList
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Способ добавления узла в начале заполнения
+    def insertAtBegin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    # Способ добавления узла с любым индексом
+    # Индексация начинается с 0.
+    def insertAtIndex(self, data, index):
+        if (index == 0):
+            self.insertAtBegin(data)
+            
+        position = 0
+        current_node = self.head
+        while (current_node != None and position+1 != index):
+            position += 1
+            current_node = current_node.next
+
+        if current_node != None:
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node
+        else:
+            print("Index not present")
+
+    # Способ добавления узла в конце LL
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while(current_node.next):
+            current_node = current_node.next
+
+        current_node.next = new_node
+
+    # Обновить узел связанного списка
+        # в заданном положении
+    def updateNode(self, val, index):
+        current_node = self.head
+        position = 0
+        if position == index:
+            current_node.data = val
+        else:
+            while(current_node != None and position != index):
+                position = position+1
+                current_node = current_node.next
+
+            if current_node != None:
+                current_node.data = val
+            else:
+                print("Index not present")
+
+    # Способ удаления первого узла связанного списка
+
+    def remove_first_node(self):
+        if(self.head == None):
+            return
+
+        self.head = self.head.next
+
+    # Способ удаления последнего узла связанного списка
+    def remove_last_node(self):
+
+        if self.head is None:
+            return
+
+        current_node = self.head
+        while(current_node != None and current_node.next.next != None):
+            current_node = current_node.next
+
+        current_node.next = None
+
+    # Метод удаления по заданному индексу
+    def remove_at_index(self, index):
+        if self.head == None:
+            return
+
+        current_node = self.head
+        position = 0
+        if position == index:
+            self.remove_first_node()
+        else:
+            while(current_node != None and position+1 != index):
+                position = position+1
+                current_node = current_node.next
+
+            if current_node != None:
+                current_node.next = current_node.next.next
+            else:
+                print("Index not present")
+
+    # Способ удаления узла из связанного спискаt
+    def remove_node(self, data):
+        current_node = self.head
+
+        if current_node.data == data:
+            self.remove_first_node()
+            return
+
+        while(current_node != None and current_node.next.data != data):
+            current_node = current_node.next
+
+        if current_node == None:
+            return
+        else:
+            current_node.next = current_node.next.next
+
+    # Напечатать размер связанного списка
+    def sizeOfLL(self):
+        size = 0
+        if(self.head):
+            current_node = self.head
+            while(current_node):
+                size = size+1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
+
+    # способ печати связанного списка
+    def printLL(self):
+        current_node = self.head
+        while(current_node):
+            print(current_node.data)
+            current_node = current_node.next
+
+
+# создайте новый связанный список
+llist = LinkedList()
+
+# добавить узлы в связанный список
+llist.insertAtEnd('a')
+llist.insertAtEnd('b')
+llist.insertAtBegin('c')
+llist.insertAtEnd('d')
+llist.insertAtIndex('g', 2)
+
+# распечатать связанный список
+print("Node Data")
+llist.printLL()
+
+# удалить узлы из связанного списка
+print("\nRemove First Node")
+llist.remove_first_node()
+print("Remove Last Node")
+llist.remove_last_node()
+print("Remove Node at Index 1")
+llist.remove_at_index(1)
+
+# распечатайте связанный список еще раз.
+print("\nLinked list after removing a node:")
+llist.printLL()
+
+print("\nUpdate node Value")
+llist.updateNode('z', 0)
+llist.printLL()
+
+print("\nSize of linked list :", end=" ")
+print(llist.sizeOfLL())
